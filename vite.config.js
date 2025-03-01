203ab2b824c9990cf486fb8a8a5aa02d7fb0b799
zhouyang <1141614570@qq.com>
2025-03-01 21:47:37 +0800

    fix: bug修复

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // 获取当前文件的目录名
 const __filename = fileURLToPath(import.meta.url);
@@ -9,13 +9,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // 设置为相对路径或你的部署路径
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 将 `@` 别名指向项目根目录下的 `src` 文件夹
-    },
-    loader: { '.js': 'jsx' }, // 明确指定 .js 文件使用 "jsx" 加载器
-  },
   css: {
     preprocessorOptions: {
       less: {
@@ -23,4 +18,10 @@ export default defineConfig({
       },
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 将 `@` 别名指向项目根目录下的 `src` 文件夹
+    },
+    loader: { ".js": "jsx" }, // 明确指定 .js 文件使用 "jsx" 加载器
+  },
+});
