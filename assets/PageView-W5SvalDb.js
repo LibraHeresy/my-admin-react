5d412edd6bf477e3ec5029714f5fa49876d41359
LibraHeresy <43395850+LibraHeresy@users.noreply.github.com>
2025-03-17 07:09:20 +0000

    Deploying to gh-pages from @ LibraHeresy/my-admin-react@15eebf774cd03535e392a31145decdb2ab0d22eb 🚀

---
+++ assets/PageView-W5SvalDb.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-DvwZ1ndD.js";const r=()=>t.jsx(e,{});export{r as default};
--- assets/order-Yoz0AtlR.js
+++
@@ -1,1 +0,0 @@
-import{ag as o,ah as t}from"./index-Dht2KiZl.js";const d=o(t(r=>({order:{orderNo:"20200801-0003",orderAmount:"300.00",orderStatus:"已支付",orderDate:"2020-08-01",orderType:"采购单",worker:"王五",orderPayment:"支付宝"},setOrder:e=>r(()=>({order:e}))}),{name:"order-storage"}));export{d as u};
